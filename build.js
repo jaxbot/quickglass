e629b0c722df2c93e34fbcd2d108792470c951fb
Jonathan Warner <jaxbot@gmail.com>
2014-04-06 13:01:29 -0400

    Working manipulation system

--- build.js
+++ build.js
@@ -1,9 +1,20 @@
 var spawn = require('child_process').spawn,
+	fs = require('fs'),
 	prepare = spawn('./prepare.sh');
 
 prepare.stdout.on('data', function(data) {
-	var tmpdir = data.toString(),
-	    build = spawn('./build.sh', [tmpdir.replace(/\n/,'')]);
+	var tmpdir = data.toString().replace(/\n/,'');
+
+	// manipulate the temporary files
+	var strings = tmpdir + "/input/res/values/values.xml";
+	var stringxml = fs.readFileSync(strings).toString();
+
+	stringxml = stringxml.replace("{{APPNAME}}", "Some name");
+	stringxml = stringxml.replace("{{VOICETRIGGER}}", "some glass example");
+
+	fs.writeFileSync(strings, stringxml);
+	
+	var build = spawn('./build.sh', [tmpdir]);
 
 	build.stdout.on('data', function(data) {
 		console.log(data.toString());
