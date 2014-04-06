9e87913d97a61ecdb9a97684a466511f382bd96a
Jonathan Warner <jaxbot@gmail.com>
2014-04-05 22:46:57 -0400

    Begin abstracting out build script

--- build.sh
+++ build.sh
@@ -1,8 +1,19 @@
+#!/bin/bash
+
+# Copy over the latest version of QuickGlass's compiled core
 cp -R -f app/quickglass/build/assets input/
 cp -R -f app/quickglass/build/manifests/debug/* input/
 cp -R -f app/quickglass/build/res/all/debug/* input/res/
 
-aapt package -v -f --no-crunch -M input/AndroidManifest.xml -I `cat .android.jar` -A input/assets --auto-add-overlay --generate-dependencies -S input/res -F output.apk app/quickglass/build/dex/debug
+# Make a temp directory to store our stuff in
+TMPDIR=$(mktemp -d)
 
-jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ~/.android/debug.keystore output.apk androiddebugkey -storepass android
+# Copy our manifest over to the temp dir for manipulation
+cp input/AndroidManifest.xml $TMPDIR/
+
+# Package using these resources
+aapt package -v -f --no-crunch -M $TMPDIR/AndroidManifest.xml -I `cat .android.jar` -A input/assets --auto-add-overlay --generate-dependencies -S input/res -F $TMPDIR/output.apk app/quickglass/build/dex/debug
+
+# Sign the new package with a debug key
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ~/.android/debug.keystore $TMPDIR/output.apk androiddebugkey -storepass android
 
