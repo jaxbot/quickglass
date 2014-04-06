1c82e7e1ff95908e6c909fb9d8be5cf6b4f81728
Jonathan Warner <jaxbot@gmail.com>
2014-04-06 12:36:10 -0400

    Reorder the jarsigner commands
    For some reason, the wrong order worked on the jarsigner on my other
    computer.

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@
 aapt package -v -f --no-crunch -M $1/input/AndroidManifest.xml -I `cat .android.jar` -A $1/input/assets --auto-add-overlay --generate-dependencies -S $1/input/res -F $1/output.apk app/quickglass/build/dex/debug
 
 # Sign the new package with a debug key
-jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ~/.android/debug.keystore $1/output.apk androiddebugkey -storepass android
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ~/.android/debug.keystore -storepass android $1/output.apk androiddebugkey
 
