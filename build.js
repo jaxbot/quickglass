fd4e8a00f81936c12fe7c67595db20ed664309f7
Jonathan Warner <jaxbot@gmail.com>
2014-04-05 23:09:38 -0400

    Basic workflow implementation

---
+++ build.js
@@ -0,0 +1,15 @@
+var spawn = require('child_process').spawn,
+	prepare = spawn('./prepare.sh');
+
+prepare.stdout.on('data', function(data) {
+	var tmpdir = data.toString(),
+	    build = spawn('./build.sh', [tmpdir]);
+
+	build.stdout.on('data', function(data) {
+		console.log(data.toString());
+	});
+	build.stderr.on('data', function(data) {
+		console.log(data.toString());
+	});
+});
+
