efc751535e8c2fd2548b61c60c31bf9ef8b21751
Jonathan Warner <jaxbot@gmail.com>
2014-04-06 22:59:15 -0400

    Clean up

--- app/quickglass/src/main/java/com/jaxbot/glass/quickglass/MainActivity.java
+++ app/quickglass/src/main/java/com/jaxbot/glass/quickglass/MainActivity.java
@@ -2,28 +2,26 @@ package com.jaxbot.glass.quickglass;
 
 import android.app.Activity;
 import android.view.View;
-import android.os.BatteryManager;
 import android.os.Bundle;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.BroadcastReceiver;
 import android.content.res.Resources;
-import android.speech.tts.TextToSpeech;
 
 import java.io.InputStream;
 
+import android.view.WindowManager;
 import android.webkit.WebView;
-
+import android.graphics.Color;
 import com.google.android.glass.app.Card;
 
 public class MainActivity extends Activity {
-	private TextToSpeech tts;
 	WebView webview;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+		super.onCreate(savedInstanceState);
 
 		getWindow().addFlags(WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
 
@@ -33,33 +31,15 @@ public class MainActivity extends Activity {
 		// see: http://stackoverflow.com/questions/9476151/webview-flashing-with-white-background-if-hardware-acceleration-is-enabled-an
 		webview.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
 		webview.setBackgroundColor(Color.argb(1, 0, 0, 0));
-        
-        tts = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
-            @Override
-            public void onInit(int status) {
-                // Do nothing.
-				tts.speak(getResources().getText(R.string.app_name).toString(), TextToSpeech.QUEUE_FLUSH, null);
-            }
-        });
 	}
 
 	@Override
 	protected void onStart() {
 		super.onStart();
-		
-		// Display it.
-		/*Card card = new Card(this);
-		card.setText(getResources().getText(R.string.app_name).toString());
-		View cardView = card.toView();
-		setContentView(cardView);
-		*/
 
-		WebView webview = new WebView(this);
 		setContentView(webview);
-
 		webview.getSettings().setJavaScriptEnabled(true);
 
-		//webview.loadUrl("http://google.com/");
 		try {
 			InputStream input = getResources().openRawResource(R.raw.card);
 			byte[] buffer = new byte[input.available()];
@@ -68,22 +48,21 @@ public class MainActivity extends Activity {
 
 			webview.loadData(data, "text/html", null);
 		} catch (Exception e) {
-			webview.loadData("<b>" + e.toString() + "</b>", "text/html", null);
+			webview.loadData("Error: <b>" + e.toString() + "</b>", "text/html", null);
 			e.printStackTrace();
 		}
-
 	}
-	
+
 	@Override
 	protected void onStop() {
 		super.onStop();
 	}
-	
+
 	@Override
-    public void onDestroy() {
+	public void onDestroy() {
 		tts.shutdown();
-        
-        super.onDestroy();
+
+		super.onDestroy();
 	}
 }
 
